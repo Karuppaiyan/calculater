c7487268444d451b7d33ffc6a0fd83174369ae65
Karuppaiyan <ukaruppaiyan@gmail.com>
2018-09-29 04:58:01 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  const value = "";	
+		addValues(){				
+			this.result1 = parseFloat(this.value * 0.90 / 100).toFixed(2);
+			this.result2 = parseFloat(this.value * 1.00 / 100).toFixed(2);
+			this.result3 = parseFloat(this.value * 1.20 / 100).toFixed(2);
+		  }
+		
+  ngOnInit(){}
+ 
 }
